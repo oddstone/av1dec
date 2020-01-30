f2b34767002bcae5df16b3e46b6511d04e41bdbb
oddstone <oddstone@gmail.com>
2020-01-31 00:14:50 +0800

    av1dec: add fps printout

--- tests/av1dec.cpp
+++ tests/av1dec.cpp
@@ -1,6 +1,8 @@
 #include "Av1Decoder.h"
 #include "VideoFrame.h"
 #include "decodeinput.h"
+#include <chrono>
+#include <iostream>
 #include <string.h>
 
 void usage(const char* app)
@@ -27,6 +29,78 @@ void writeFrame(FILE* fp, std::shared_ptr<Yami::YuvFrame>& frame)
     fflush(fp);
 }
 
+using std::chrono::duration;
+using std::chrono::system_clock;
+using std::chrono::time_point;
+class Fps {
+    enum Tag {
+        READ,
+        WRITE,
+        DECODE,
+        COUNT_OF_TAG,
+    };
+
+public:
+    void startRead()
+    {
+        start(READ);
+    }
+    void endRead()
+    {
+        end(READ);
+    }
+    void startDecode()
+    {
+        start(DECODE);
+    }
+    void endDecode()
+    {
+        end(DECODE);
+    }
+    void startWrite()
+    {
+        start(WRITE);
+    }
+    void endWrite()
+    {
+        end(WRITE);
+    }
+    void fps()
+    {
+        std::cout << "decode fps: " << getFps(DECODE) << "\r";
+    }
+    void summary()
+    {
+        std::cout << "decode fps = " << getFps(DECODE)
+                  << ", read fps = " << getFps(READ)
+                  << ", write fps = " << getFps(WRITE) << std::endl;
+    }
+
+private:
+    float getFps(Tag tag)
+    {
+        float d = m_duration[tag].count();
+        if (d > 0) {
+            return count[tag] / d;
+        }
+        return 0;
+    }
+    void start(Tag tag)
+    {
+        m_start[tag] = system_clock::now();
+    }
+    void end(Tag tag)
+    {
+        duration<float> d = system_clock::now() - m_start[tag];
+        m_duration[tag] += d;
+        count[tag]++;
+    }
+
+    time_point<system_clock> m_start[COUNT_OF_TAG];
+    duration<float> m_duration[COUNT_OF_TAG];
+    int count[COUNT_OF_TAG];
+};
+
 int main(int argc, char** argv)
 {
     if (argc != 4 || strcmp(argv[1], "-i")) {
@@ -46,14 +120,28 @@ int main(int argc, char** argv)
 
     VideoDecodeBuffer buf;
     YamiAv1::Decoder decoder;
-    while (input->getNextDecodeUnit(buf)) {
-        printf("%d\r\n", buf.size);
+    Fps fps;
+    while (1) {
+        //printf("%d\r\n", buf.size);
+        fps.startRead();
+        if (!input->getNextDecodeUnit(buf))
+            break;
+        fps.endRead();
+
+        fps.startDecode();
         decoder.decode(buf.data, buf.size);
+        fps.endDecode();
+
+        fps.fps();
+
         std::shared_ptr<Yami::YuvFrame> frame;
         while ((frame = decoder.getOutput())) {
+            fps.startWrite();
             writeFrame(out, frame);
+            fps.endWrite();
         }
     }
+    fps.summary();
     fclose(out);
     //getchar();
     return 0;
